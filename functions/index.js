ae12a848e28fb9a3c3705d615d83d2cf9f9eda0b
CARD1NAL <cardinal312@github.com>
2024-10-19 16:24:56 +0600

    Refactored code

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,36 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+/* eslint-disable max-len */
+const functions = require("firebase-functions/v1");
+const admin = require("firebase-admin");
+admin.initializeApp({
+	credential: admin.credential.applicationDefault(),
+});
+exports.getNewMessage = functions.firestore.document("users/{userId}/conversations/{messageId}")
+	.onCreate(async (doc, context) => {
+	  const db = admin.firestore();
+	  const date = doc.data().date;
+      const otherId = doc.data().otherId;
+	  const userId = context.params.userId;
+	  let userToken = "";
+	  try {
+		const data = await db.collection("users").doc(userId).get();
+		const token = data.data().token;
+		  userToken = token;
+	  } catch (error) {
+		console.log(error);
+	  }
+	  const message = {
+		notification: {
+		  title: date,
+		  body: otherId,
+		},
+		token: userToken,
+	  };
+	  admin.messaging().send(message)
+		  .then((responce) => {
+		    console.log(responce);
+		  })
+		  .catch((error) => {
+		    console.log(error);
+		  });
+	});
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
